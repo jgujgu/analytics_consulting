1f02db50e0a557e6f838af6809347827068cb72c
jgu <jgu@jeffreygu.com>
2016-06-26 17:31:30 -0600

    fix email sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,20 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
   config.serve_static_assets = true
+  config.action_mailer.default_url_options = {
+     :host => 'lvh.me',
+     :port => 3000
+   }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
+    enable_starttls_auto: true
+  }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
