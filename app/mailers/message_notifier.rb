2df526207d1014eeaaf1255f23e680940adfbb47
jgu <jgu@jeffreygu.com>
2016-06-26 19:25:26 -0600

    enable sean email

--- app/mailers/message_notifier.rb
+++ app/mailers/message_notifier.rb
@@ -2,7 +2,7 @@ class MessageNotifier < ApplicationMailer
   def send_message(email, message)
     @message = message
     emails = [
-      #ENV["sean_email"],
+      ENV["sean_email"],
       ENV["jeff_email"]
     ]
     mail( :to => emails,
